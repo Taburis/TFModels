6c2e51a27dc2e5eb3fed46b2ba833e3511f85de1
TabrisXiao <tabris.xiao@gmail.com>
2021-04-06 01:13:10 -0500

    changed the tab format

--- blocks.py
+++ blocks.py
@@ -3,36 +3,36 @@ import layers as xlayers
 import tensorflow as tf
 
 def sequential_block(nlayer, module, cfg, trainable):
-	""" stacks sub-blocks to form a new sequential blocks
-	"""
-	module_fn = module(cfg, trainable)	
-	def sequential_block_imp(inputs):
-		x = inputs
-		for _ in range(nlayer):
-			x = module_fn(x)
-		return x
-	return sequential_block_imp
+    """ stacks sub-blocks to form a new sequential blocks
+    """
+    module_fn = module(cfg, trainable)	
+    def sequential_block_imp(inputs):
+        x = inputs
+        for _ in range(nlayer):
+            x = module_fn(x)
+        return x
+    return sequential_block_imp
 
 def sequential_conv2d(cfg, trainable):
-	"""stacked 2D convolution layers
-	cfg {
-		conv2d : 'dict', conv2d configuration;
-		trainable : 'bool', if this block is trainable;
-		nlayer : 'int', number of conv2d for stacking;
-		use_residual: 'bool', if the residual learning will be added,
-					the shortcut will connect the input and the output
-	}
-	"""
-	cfg_conv2d = cfg.conv2d
-	cfg_conv2d['trainable'] = trainable
-	nlayer= cfg.nlayer
-	use_residual = cfg.residual_learning
-	def sequential_conv2d_imp(inputs):
-		x = inputs
-		shortcut = inputs
-		for i in range( nlayer):
-			x = tf.keras.layers.Conv2D(**cfg_conv2d)(x)
-		if use_residual : 
-			x = xlayers.residual2D_layer(trainable = trainable)(shortcut, x)
-		return x
-	return sequential_conv2d_imp
+    """stacked 2D convolution layers
+    cfg {
+        conv2d : 'dict', conv2d configuration;
+        trainable : 'bool', if this block is trainable;
+        nlayer : 'int', number of conv2d for stacking;
+        use_residual: 'bool', if the residual learning will be added,
+                    the shortcut will connect the input and the output
+    }
+    """
+    cfg_conv2d = cfg.conv2d
+    cfg_conv2d['trainable'] = trainable
+    nlayer= cfg.nlayer
+    use_residual = cfg.residual_learning
+    def sequential_conv2d_imp(inputs):
+        x = inputs
+        shortcut = inputs
+        for i in range( nlayer):
+            x = tf.keras.layers.Conv2D(**cfg_conv2d)(x)
+        if use_residual : 
+            x = xlayers.residual2D_layer(trainable = trainable)(shortcut, x)
+        return x
+    return sequential_conv2d_imp
--- factory.py
+++ factory.py
@@ -6,27 +6,27 @@ from __future__ import print_function
 import tensorflow as tf
 
 class factory(object):
-	def __init__(self, model):
-		self.model = model
-		self.outputs = model.outputs
-	
-	def loss(self, y_true, y_pred):
-		return self.loss_fn(y_true= y_true, y_pred = y_pred)
-
-	def training_iter_step(self, inputs):
-		x, labels = inputs
-		
-		with tf.GradientTape() as tape:
-			y_ = self.model(x)
-			loss = self.loss_fn(y_true= labels, y_pred = y_)
-		
-		grads = tape.gradient(loss, self.model.trainable_variables)
-		self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-		return loss
-
-	def training_step(self, iterator, num_steps):
-		if not isinstance(num_steps, tf.Tensor):
-			num_steps = tf.convert_to_tensor(num_steps, dtype = tf.int32)
-		for _ in tf.range(num_steps -1):
-			print(self.training_iter_step(next(iterator)))
-		return
+    def __init__(self, model):
+        self.model = model
+        self.outputs = model.outputs
+    
+    def loss(self, y_true, y_pred):
+        return self.loss_fn(y_true= y_true, y_pred = y_pred)
+    
+    def training_iter_step(self, inputs):
+        x, labels = inputs
+        
+        with tf.GradientTape() as tape:
+            y_ = self.model(x)
+            loss = self.loss_fn(y_true= labels, y_pred = y_)
+        
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        return loss
+    
+    def training_step(self, iterator, num_steps):
+        if not isinstance(num_steps, tf.Tensor):
+            num_steps = tf.convert_to_tensor(num_steps, dtype = tf.int32)
+        for _ in tf.range(num_steps -1):
+            print(self.training_iter_step(next(iterator)))
+        return
