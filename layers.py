ac79dcaa881bdc9a4ef8daaf960e341d396740f4
TabrisXiao <tabris.xiao@gmail.com>
2021-04-06 01:07:27 -0500

    added CAM as a header

--- layers.py
+++ layers.py
@@ -1,4 +1,12 @@
 
+"""
+collection of customed layers
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
 
 class residual2D_layer(tf.keras.layers.Layer):
--- templates/resnet.py
+++ templates/resnet.py
@@ -1,4 +1,23 @@
 
+
+"""
+Template for Residual Networks
+
+based on the paper https://arxiv.org/pdf/1512.03385.pdf
+
+pre_build will generate a default cfg files consumed by build_template.
+	cfg = pre_build('34') or '50'
+Adjust the configuration `cfg` and the function
+	build_template(cfg)
+returns the decorated resnet callable function.
+
+notice, the normalization layer is not added for residual shortcut
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import cfg_base as cfgb
 import layers as xlayers
 import blocks as xblocks
@@ -11,7 +30,8 @@ def pre_build(ids, trainable = True):
 	"""default ResNet 34 or 50
 	"""
 	features = 64 # root features of the first block, it will be doubled for each block
-	nsublayers = 3
+	nsublayers = 3 # usually the structure that shortcut 3 sublayers for residual is called
+				   # bottle neck. But resnet with depth 34, no bottle neck is used. 
 	if ids == '34' : 
 		nsublayers = 2
 	default_cfg_conv2d = {'filters':64,
@@ -38,7 +58,7 @@ def pre_build(ids, trainable = True):
 	cfg_bn_4 = cfg_bottle_neck.clone()
 	cfg_bn_4.conv2d['filters'] = features*8
 	cfg = [cfg_bn_1, cfg_bn_2,cfg_bn_3,cfg_bn_4]
-	nlayers = [3, 4, 6, 3]
+	nlayers = [3, 4, 6, 3] # the numbers of shortcuts in 34 and 50 resnet are the same 
 	return cfg, nlayers
 
 def build_template(cfg_resnet, trainable=True):
